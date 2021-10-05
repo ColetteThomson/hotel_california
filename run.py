75f4beb4fecb9488fdbe8ae3037fad1050f2f19b
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-10-05 11:54:32 +0000

    add hotel welcome message

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 from datetime import datetime
+
+# Hotel welcome and information message
+print('\n*** WELCOME TO THE HOTEL CALIFORNIA ***')
+print('\nYou have reached our quick and easy')
+print('online booking system.')
+print('Please enter your details as prompted.')
+print('You can call us on 0090-1234567')
+print('should you have any queries.\n')
